396912eae152eea9c6be58abb03136e7d5defb64
Ondřej Mastík <ondramastik@outlook.com>
2015-09-12 02:39:47 +0200

    Added on github

---
+++ Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        private int posX;
+        private int posY;
+
+        public Point(int X, int Y)
+        {
+            this.posX = X;
+            this.posY = Y;
+        }
+        public void Set(Point input)
+        {
+            this.posX = input.X;
+            this.posY = input.Y;
+        }
+
+        public int X
+        {
+            get { return this.posX; }
+            set { this.posX = value; }
+        }
+        public int Y
+        {
+            get { return this.posY; }
+            set { this.posY = value; }
+        }
+        public override string ToString()
+        {
+            return "[" + this.posX + ", " + this.posY + "]";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Snake
+{
+    class Program
+    {
+        public static GameField hra;
+        static void Control()
+        {
+            while (hra.isRunning)
+            {
+                ConsoleKeyInfo a = Console.ReadKey();
+                ConsoleKey key = a.Key;
+                if (key.ToString().Contains("A")) { if (hra.direction != 2) hra.direction = 1; }
+                else if (key.ToString().Contains("D")) { if (hra.direction != 1) hra.direction = 2; }
+                else if (key.ToString().Contains("W")) { if (hra.direction != 4) hra.direction = 3; }
+                else if (key.ToString().Contains("S")) { if (hra.direction != 3) hra.direction = 4; }
+                Thread.Sleep(100);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            hra = new GameField(20, 20);
+            Thread thread = new Thread(new ThreadStart(Control));
+            hra.Start();
+            thread.Start();
+
+            while (hra.isRunning)
+            {
+                Console.SetCursorPosition(0,0);
+                Console.Write(hra.mapToString());
+                hra.updateSnake();
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
